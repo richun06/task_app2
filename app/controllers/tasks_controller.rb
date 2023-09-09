c60779304a9ca23ec8cd1a7c3e6a5f48abbaa520
rika kaminoyama <richun.06@gmail.com>
2023-09-09 17:31:55 +0900

    タスク新規作成機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,24 @@
 class TasksController < ApplicationController
-
   def index
     
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスク新規作成完了"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
   end
 end
